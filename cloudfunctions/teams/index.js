39ce8fafd44e9742dc2edb44402e3c857c2ecbeb
liulxin <550293874@qq.com>
2019-12-07 18:08:03 +0800

    chance

--- cloudfunctions/teams/index.js
+++ cloudfunctions/teams/index.js
@@ -9,6 +9,6 @@ const $ = _.aggregate
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  let res = db.collection('teams').skip(event.skip * 15).limit(15).get()
+  let res = db.collection('teams').aggregate().sort({ 'line_id':-1}).skip(event.skip * 15).limit(15).end()
   return res
 }
